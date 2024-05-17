754363cab5a1e341e65b66f0e21f6492fb4397f5
grzegorzgr <g.gruszecki@kainos.com>
2024-05-17 13:07:27 +0200

    env test
    tests
    
    updated

--- ops/docker/pipeline_vars.sh
+++ ops/docker/pipeline_vars.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/env bash
+set -e
 
 NAMESPACE=$1
 
@@ -6,4 +7,7 @@ export API_GATEWAY_URL=http://api-gateway-svc.${NAMESPACE}.svc:8080
 export KAFKA_BROKER=kafka-main-headless.${NAMESPACE}.svc:9092
 export KAFKA_SCHEMA_REGISTRY=http://schema-registry-svc.${NAMESPACE}.svc:8081
 export PETSTORE_STUB_URL=http://petstore-stub-svc.${NAMESPACE}.svc:8080
+<<<<<<< HEAD
 export SFDC_STUB_URL=http://sfdc-stub-svc.${NAMESPACE}.svc:8080
+=======
+>>>>>>> 7b3a334 (updated)
