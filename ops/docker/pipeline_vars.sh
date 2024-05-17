f2246c7d1b8ce3ef1ee948cbbe7ccba507a98c2c
Grzegorz Gruszecki <root@ZephyrusG14>
2024-05-17 14:35:59 +0200

    added proper value setup

--- ops/docker/pipeline_vars.sh
+++ ops/docker/pipeline_vars.sh
@@ -2,10 +2,7 @@
 
 NAMESPACE=$1
 
-API_GATEWAY_URL=http://api-gateway-svc.${NAMESPACE}.svc:8080
-KAFKA_KEY_DESERIALIZER=org.apache.kafka.common.serialization.StringDeserializer
-KAFKA_VALUE_DESERIALIZERr=io.confluent.kafka.serializers.KafkaAvroDeserializer
-KAFKA_AUTO_OFFSET_RESET=earliest
-KAFKA_BROKER=kafka-main-headless.${NAMESPACE}.svc:9092
-KAFKA_SCHEMA_REGISTRY=http://schema-registry-svc.${NAMESPACE}.svc:8081
-PETSTORE_STUB_URL=http://petstore-stub-svc.${NAMESPACE}.svc:8080
+export API_GATEWAY_URL=http://api-gateway-svc.${NAMESPACE}.svc:8080
+export KAFKA_BROKER=kafka-main-headless.${NAMESPACE}.svc:9092
+export KAFKA_SCHEMA_REGISTRY=http://schema-registry-svc.${NAMESPACE}.svc:8081
+export PETSTORE_STUB_URL=http://petstore-stub-svc.${NAMESPACE}.svc:8080
