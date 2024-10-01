37ac26cf9b06b41e19383f356cb39225501f2a60
ka-Grzegorz.Gruszecki <ka.grzegorz.gruszecki@nhsbsa.nhs.uk>
2024-10-01 14:29:41 +0200

    forcepush

--- ops/docker/pipeline_vars.sh
+++ ops/docker/pipeline_vars.sh
@@ -7,7 +7,4 @@ export API_GATEWAY_URL=http://api-gateway-svc.${NAMESPACE}.svc:8080
 export KAFKA_BROKER=kafka-main-headless.${NAMESPACE}.svc:9092
 export KAFKA_SCHEMA_REGISTRY=http://schema-registry-svc.${NAMESPACE}.svc:8081
 export PETSTORE_STUB_URL=http://petstore-stub-svc.${NAMESPACE}.svc:8080
-<<<<<<< HEAD
 export SFDC_STUB_URL=http://sfdc-stub-svc.${NAMESPACE}.svc:8080
-=======
->>>>>>> 7b3a334 (updated)
