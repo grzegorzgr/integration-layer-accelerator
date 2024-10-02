48a824c03d2b6ec38641249b011d9e5af071c710
grzegorzgr <g.gruszecki@kainos.com>
2024-10-02 16:35:05 +0200

    Added AEP Reviewer and Summary generator (#10)
    * Added AEP Reviewer and Summary generator
    
    * fixed branch for which pipeline is being triggered
    
    * updated vars
    
    ---------
    
    Co-authored-by: Grzegorz Gruszecki <root@ZephyrusG14>

--- ops/docker/pipeline_vars.sh
+++ ops/docker/pipeline_vars.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-
+#!/bin/env bash
 NAMESPACE=$1
 
 export API_GATEWAY_URL=http://api-gateway-svc.${NAMESPACE}.svc:8080
-export KAFKA_BROKER=kafka-main-headless.${NAMESPACE}.svc:9092
+export KAFKA_BROKER=kafka-${NAMESPACE}-headless.${NAMESPACE}.svc:9092
 export KAFKA_SCHEMA_REGISTRY=http://schema-registry-svc.${NAMESPACE}.svc:8081
 export PETSTORE_STUB_URL=http://petstore-stub-svc.${NAMESPACE}.svc:8080
 export SFDC_STUB_URL=http://sfdc-stub-svc.${NAMESPACE}.svc:8080
